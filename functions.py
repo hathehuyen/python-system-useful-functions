26a7e142c0506366963519add912fb121d60ce3e
huyenha <hathehuyen@gmail.com>
2017-02-18 14:54:30 +0700

    Add check last modified time of a url

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 from urllib2 import urlopen
+from datetime import datetime
+from rfc822 import parsedate_tz
 
 
 # Download url to file with buffered read and progress display
@@ -24,3 +26,13 @@ def download(url, file_name):
         print status,
 
     f.close()
+
+
+# Check last modified time of an url using Last-Modified header
+def check_last_modified(url):
+    u = urlopen(url)
+    meta = u.info()
+    last_modified = meta.getheaders("Last-Modified")[0]
+    # modified = datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S GMT')
+    modified = datetime(*parsedate_tz(last_modified)[:7])
+    return modified
