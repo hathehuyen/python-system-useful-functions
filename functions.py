64eb6a4560ffe27703fe0c731ad6851cc59fefba
huyenha <hathehuyen@gmail.com>
2017-02-18 14:52:00 +0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from urllib2 import urlopen
+
+
+# Download url to file with buffered read and progress display
+def download(url, file_name):
+    u = urlopen(url)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8) * (len(status) + 1)
+        print status,
+
+    f.close()
